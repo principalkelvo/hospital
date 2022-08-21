bcab66262409dfccf8fe8a980af0e63bc3147a36
principalkelvo <kelvinnjoroowino@gmail.com>
2022-08-21 09:41:40 +0300

    simple home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,46 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+    <section class="hero">
+      <div class="hero-body">
+        <div class="">
+          <h2 class="title has-text-weight-bold has-text-black is-size-4">
+            Orders
+          </h2>
+          <div class="columns is-fullwidth has-background-white box">
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4 has-text-centered"
+              >
+                Active Orders
+              </h2>
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4 has-text-centered"
+              >
+                Shipped orders
+              </h2>
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4 has-text-centered"
+              >
+                Returned Orders
+              </h2>
+
+            </div>
+            <div class="column m-2 box has-background-faded-pink">
+              <h2
+                class="is-size-6 has-text-weight-bold has-text-black is-size-4 has-text-centered"
+              >
+                Closed orders
+              </h2>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
