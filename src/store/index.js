8093dd35cf1ca26f969eb269c92b57ea3ed6334f
principalkelvo <kelvinnjoroowino@gmail.com>
2022-08-21 09:41:53 +0300

    import images

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,57 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+          //loading
+          isLoading: true,
+
+          // authentication 
+          isAuthenticated: false,
+          token: '',
+  
+          user: {
+              id: 0,
+              username: '',
+          }
   },
   getters: {
   },
   mutations: {
+       // to know if we had logged in
+       initializeStore(state) {
+        // if yes
+        if (localStorage.getItem('token')) {
+            state.token = localStorage.getItem('token')
+            state.isAuthenticated = true
+
+            state.user.username = localStorage.getItem('username')
+            state.user.id = localStorage.getItem('user_id')
+        } //if not logged in
+        else {
+            state.token = ''
+            state.isAuthenticated = false
+            state.user.id = 0
+            state.user.username = ''
+        }
+    },
+
+    // change of loading status
+    setIsLoading(state, status) {
+        state.isLoading = status
+    },
+    // function to set token
+    setToken(state, token) {
+        state.token = token
+        state.isAuthenticated = true
+    },
+    // function to sign out
+    removeToken(state) {
+        state.token = ''
+        state.isAuthenticated = false
+    },
+
+    setUser(state, user) {
+        state.user = user
+    }
   },
   actions: {
   },
