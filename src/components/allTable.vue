4667dac79a51356995a449303c5c533f2409bb05
principalkelvo <kelvinnjoroowino@gmail.com>
2022-08-21 12:03:52 +0300

    not working

--- src/components/allTable.vue
+++ src/components/allTable.vue
@@ -4,18 +4,31 @@
       {{ title }}
     </h2>
     <div class="column is-12 is-fullwidth box">
-      <input type="text" class="input is-small is-rounded column is-4 mb-2" placeholder="&#xebf7; Search..."
-        v-model="searchTerm" />
-      <vue-good-table :columns="columns" :rows="rows" :line-numbers="true" max-height="500px" :search-options="{
-        enabled: true,
-        externalQuery: searchTerm,
-      }" :sort-options="{
-  enabled: false,
-}" :pagination-options="{
-  enabled: true,
-  mode: 'pages',
-  perPage: 5,
-}" styleClass="vgt-table condensed" compactMode class="has-text-centered">
+      <input
+        type="text"
+        class="input is-small is-rounded column is-4 mb-2"
+        placeholder="&#xebf7; Search..."
+        v-model="searchTerm"
+      />
+      <vue-good-table
+        :columns="columns"
+        :rows="rows"
+        :line-numbers="true"
+        max-height="500px"
+        :search-options="{
+          enabled: true,
+          externalQuery: searchTerm,
+        }"
+       
+        :pagination-options="{
+          enabled: true,
+          mode: 'pages',
+          perPage: 5,
+        }"
+        styleClass="vgt-table condensed"
+        compactMode
+        class="has-text-centered"
+      >
         <!-- format images -->
         <template #table-row="props">
           <span v-if="props.column.field == 'get_image'">
@@ -30,20 +43,36 @@
 
           <span v-if="props.column.field == 'btn'">
             <span class="icon mr-2 is-clickable">
-              <router-link :to="{ name: 'Employee', params: { id: props.row.reg_no } }"><svg
-                  xmlns="http://www.w3.org/2000/svg" class="has-text-dark" width="16" fill="none" viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z">
-                  </path>
-                </svg></router-link>
+              <router-link
+                :to="{ name: 'Employee', params: { id: props.row.reg_no } }"
+                ><svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="has-text-dark"
+                  width="16"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                  ></path>
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
+                  ></path></svg
+              ></router-link>
             </span>
 
             <span class="icon is-clickable">
-              <router-link :to="{ name: 'EditEmployee', params: { id: props.row.reg_no } }"><i
-                  class="bx bx-pencil has-text-dark"></i></router-link>
+              <router-link
+                :to="{ name: 'EditEmployee', params: { id: props.row.reg_no } }"
+                ><i class="bx bx-pencil has-text-dark"></i
+              ></router-link>
               <!-- {{props.row.reg_no}} -->
               <!-- <i class="bx bx-pencil "></i> -->
             </span>
@@ -56,13 +85,18 @@
 
         <!-- add action buttons -->
         <template #table-actions>
-
           <div class="buttons">
-            <router-link to="#" class="button has-text-white has-text-weight-bold is-size-7 is-pink mx-2">
+            <router-link
+              to="#"
+              class="button has-text-white has-text-weight-bold is-size-7 is-pink mx-2"
+            >
               <i class="bx bx-chevron-down mr-2"></i>
               <span>Export</span>
             </router-link>
-            <router-link to="#" class="button has-text-white has-text-weight-bold is-size-7 is-pink mx-2">
+            <router-link
+              to="#"
+              class="button has-text-white has-text-weight-bold is-size-7 is-pink mx-2"
+            >
               <i class="bx bx-chevron-down mr-2"></i>
               <span> Add Employee </span>
             </router-link>
@@ -79,7 +113,7 @@ import axios from "axios";
 
 export default {
   name: "allTable",
-  props: ['title'],
+  props: ["title"],
   data() {
     return {
       searchTerm: "",
